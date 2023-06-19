8bbf61a113bca7fe85b868a4749c51e677a03854
Anthonylooby1 <anthonylooby@yahoo.com>
2023-06-19 18:51:52 -0400

    Button Clicker

---
+++ script.js
@@ -0,0 +1,11 @@
+function hide(element) {
+    element.remove();
+}
+
+function alertFunction() {
+    alert("Ninja was liked");
+}
+
+function changeName(element) {
+    element.innerText = "Logout";
+}
